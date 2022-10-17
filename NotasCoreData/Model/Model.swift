066eb70cbaa4b04d9081e466b4f7512b8dbf928b
caroaz <carolinaazcurra8@gmail.com>
2022-10-17 19:39:17 -0300

    coreData

---
+++ NotasCoreData/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  NotasCoreData
+//
+//  Created by CLS7377387-MOBD on 17-10-22.
+//
+
+import Foundation
---
+++ NotasCoreData/TodoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  NotasCoreData
+//
+//  Created by CLS7377387-MOBD on 17-10-22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
